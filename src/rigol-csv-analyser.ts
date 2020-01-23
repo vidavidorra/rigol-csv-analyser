2a8462ddc49fe77dd2fade172b7ed749febb6866
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-01-23 18:59:35 +0100

    feat: add parsing of the CSV header

--- src/rigol-csv-analyser.ts
+++ src/rigol-csv-analyser.ts
@@ -2,6 +2,7 @@ import { Options } from './options';
 import { Server } from './server';
 import fs from 'fs';
 import mustache from 'mustache';
+import parse from 'csv-parse/lib/sync';
 import path from 'path';
 
 export class RigolCsvAnalyser {
@@ -14,16 +15,63 @@ export class RigolCsvAnalyser {
   };
   private options: Options;
 
+  private header = {
+    increment: 0,
+    channels: [],
+  };
+
   public constructor(options: Options) {
     this.options = options;
   }
 
   public Analyse(): void {
+    this.ParseHeader();
     /**
-     * - Parse CSV
      * - Generate serveFiles.data
      */
     this.GenerateChart();
+  }
+
+  /**
+   * CSV header from a Rigol DS1054Z.
+   * X,CH1,CH2,Start,Increment,
+   * Sequence,Volt,Volt,-5.999998e-02,2.000000e-05
+   */
+  private ParseHeader(): void {
+    try {
+      const data = Buffer.alloc(200);
+      const fd = fs.openSync(this.options.csvFile, 'r');
+      fs.readSync(fd, data, 0, data.length, 0);
+      fs.closeSync(fd);
+
+      const lines = data
+        .toString()
+        .split(/\r|\n|\r\n/)
+        .slice(0, 2)
+        .map((line: string): string => {
+          return line.replace(/,$/, '');
+        });
+
+      console.log(lines);
+
+      const records = parse(lines.join('\n'), {
+        columns: false,
+        from_line: 1, // eslint-disable-line @typescript-eslint/camelcase
+      });
+
+      const incrementIndex = records[0].findIndex(
+        item => 'increment' === item.toLowerCase()
+      );
+      this.header.increment = Number(records[1][incrementIndex]);
+      this.header.channels = records[0].filter(item => item.startsWith('CH'));
+
+      console.log(`Channels: '${this.header.channels.join("', '")}'`);
+      console.log(`Increment: ${this.header.increment} s`);
+    } catch (error) {
+      console.log('Error parsing the header of the file');
+      console.log(error);
+      process.exit(1);
+    }
   }
 
   public Serve(): void {
